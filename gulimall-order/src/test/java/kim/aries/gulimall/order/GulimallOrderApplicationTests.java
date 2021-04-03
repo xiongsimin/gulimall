975070b96a307e94dfc52d57a9f37e104c3be60a
aries <2248950919@qq.com>
2021-04-03 21:40:28 +0800

    谷粒商城--项目骨架搭建

---
+++ gulimall-order/src/test/java/kim/aries/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.aries.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

105612c446f92c83fe14b0a848b96231f56d1465
aries <2248950919@qq.com>
2021-04-06 23:03:41 +0800

    引入Nacos配置中心组件并将coupon微服务配置拆分、使用Nacos配置中心管理

--- gulimall-coupon/src/main/java/kim/aries/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/kim/aries/gulimall/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +25,22 @@ import kim.aries.common.utils.R;
  * @email aries_test@qq.com
  * @date 2021-04-05 16:49:01
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("test/config")
+    public R testConfig() {
+        return R.ok().put("name", name).put("age", age);
+    }
 
     @RequestMapping("/member/list")
     public R memberCoupons() {

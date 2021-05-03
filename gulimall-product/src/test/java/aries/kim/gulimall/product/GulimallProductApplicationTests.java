9377b1f82a5594765b8c2a541af15624be8783c6
aries <2248950919@qq.com>
2021-05-03 20:06:47 +0800

    新增第三方微服务子模块、集成第三方--阿里云OOS、添加OSS签名接口

--- gulimall-product/src/test/java/aries/kim/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/aries/kim/gulimall/product/GulimallProductApplicationTests.java
@@ -1,30 +1,28 @@
 package aries.kim.gulimall.product;
 
-import com.aliyun.oss.OSS;
-import com.aliyun.oss.OSSClientBuilder;
-import com.aliyun.oss.model.PutObjectRequest;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import kim.aries.gulimall.product.GulimallProductApplication;
 import kim.aries.gulimall.product.entity.BrandEntity;
 import kim.aries.gulimall.product.service.BrandService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.File;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = GulimallProductApplication.class)
-class GulimallProductApplicationTests {
+public class GulimallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
 
+
     @Test
-    void contextLoads() {
+    public void contextLoads() {
         BrandEntity brandEntity = new BrandEntity();
         brandEntity.setBrandId(1L);
         brandEntity.setDescript("小米");
@@ -41,35 +39,6 @@ class GulimallProductApplicationTests {
         result.forEach((e) -> {
             System.out.println(e);
         });
-    }
-
-    @Test
-    public void testUpload() {
-        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
-        String endpoint = "oss-cn-beijing.aliyuncs.com";
-        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "accessKeyId";
-        String accessKeySecret = "accessKeySecret";
-
-        // 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-
-        // 创建PutObjectRequest对象。
-        // 填写Bucket名称、Object完整路径和本地文件的完整路径。Object完整路径中不能包含Bucket名称。
-        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件。
-        PutObjectRequest putObjectRequest = new PutObjectRequest("gulimall-demo-aries", "pika.png", new File("I:\\pika.png"));
-
-        // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
-        // ObjectMetadata metadata = new ObjectMetadata();
-        // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
-        // metadata.setObjectAcl(CannedAccessControlList.Private);
-        // putObjectRequest.setMetadata(metadata);
-
-        // 上传文件。
-        ossClient.putObject(putObjectRequest);
-
-        // 关闭OSSClient。
-        ossClient.shutdown();
     }
 
 }

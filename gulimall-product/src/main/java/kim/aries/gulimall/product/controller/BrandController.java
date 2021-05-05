98ea15c1bfaf74c248f272a637a482b2082f74f5
aries <2248950919@qq.com>
2021-05-05 23:05:13 +0800

    JSR303数据校验

--- gulimall-product/src/main/java/kim/aries/gulimall/product/controller/BrandController.java
+++ gulimall-product/src/main/java/kim/aries/gulimall/product/controller/BrandController.java
@@ -1,9 +1,11 @@
 package kim.aries.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import kim.aries.gulimall.product.service.BrandService;
 import kim.aries.common.utils.PageUtils;
 import kim.aries.common.utils.R;
 
+import javax.validation.Valid;
 
 
 /**
@@ -36,7 +39,7 @@ public class BrandController {
     @RequestMapping("/list")
     //生成的Controller文件中每个Controller都生成Shiro注解
     //@RequiresPermissions("product:brand:list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = brandService.queryPage(params);
 
         return R.ok().put("page", page);
@@ -48,8 +51,8 @@ public class BrandController {
      */
     @RequestMapping("/info/{brandId}")
     //@RequiresPermissions("product:brand:info")
-    public R info(@PathVariable("brandId") Long brandId){
-		BrandEntity brand = brandService.getById(brandId);
+    public R info(@PathVariable("brandId") Long brandId) {
+        BrandEntity brand = brandService.getById(brandId);
 
         return R.ok().put("brand", brand);
     }
@@ -59,9 +62,19 @@ public class BrandController {
      */
     @RequestMapping("/save")
     //@RequiresPermissions("product:brand:save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
-
+    public R save(@Valid @RequestBody BrandEntity brand, BindingResult result) {
+        if (result.hasErrors()) {
+            Map errorMap = new HashMap();
+            //获取校验的错误信息
+            result.getFieldErrors().forEach((item) -> {
+                //获取错误的属性的名字
+                String field = item.getField();
+                errorMap.put(field, item.getDefaultMessage());
+            });
+            return R.error(400, "提交的数据不合法").put("data", errorMap);
+        } else {
+            brandService.save(brand);
+        }
         return R.ok();
     }
 
@@ -70,8 +83,8 @@ public class BrandController {
      */
     @RequestMapping("/update")
     //@RequiresPermissions("product:brand:update")
-    public R update(@RequestBody BrandEntity brand){
-		brandService.updateById(brand);
+    public R update(@RequestBody BrandEntity brand) {
+        brandService.updateById(brand);
 
         return R.ok();
     }
@@ -81,8 +94,8 @@ public class BrandController {
      */
     @RequestMapping("/delete")
     //@RequiresPermissions("product:brand:delete")
-    public R delete(@RequestBody Long[] brandIds){
-		brandService.removeByIds(Arrays.asList(brandIds));
+    public R delete(@RequestBody Long[] brandIds) {
+        brandService.removeByIds(Arrays.asList(brandIds));
 
         return R.ok();
     }
--- gulimall-product/src/main/java/kim/aries/gulimall/product/entity/BrandEntity.java
+++ gulimall-product/src/main/java/kim/aries/gulimall/product/entity/BrandEntity.java
@@ -4,12 +4,15 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
 import java.io.Serializable;
-import java.util.Date;
+
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.*;
 
 /**
  * 品牌
- * 
+ *
  * @author aries
  * @email aries_test@qq.com
  * @date 2021-04-04 17:02:01
@@ -17,36 +20,43 @@ import lombok.Data;
 @Data
 @TableName("pms_brand")
 public class BrandEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 品牌id
-	 */
-	@TableId
-	private Long brandId;
-	/**
-	 * 品牌名
-	 */
-	private String name;
-	/**
-	 * 品牌logo地址
-	 */
-	private String logo;
-	/**
-	 * 介绍
-	 */
-	private String descript;
-	/**
-	 * 显示状态[0-不显示；1-显示]
-	 */
-	private Integer showStatus;
-	/**
-	 * 检索首字母
-	 */
-	private String firstLetter;
-	/**
-	 * 排序
-	 */
-	private Integer sort;
+    /**
+     * 品牌id
+     */
+    @TableId
+    private Long brandId;
+    /**
+     * 品牌名
+     */
+    @NotBlank(message = "品牌名称不能为空")
+    private String name;
+    /**
+     * 品牌logo地址
+     */
+    @NotEmpty
+    @URL(message = "logo必须是一个合法的URL地址")
+    private String logo;
+    /**
+     * 介绍
+     */
+    private String descript;
+    /**
+     * 显示状态[0-不显示；1-显示]
+     */
+    private Integer showStatus;
+    /**
+     * 检索首字母
+     */
+    @NotNull
+    @Pattern(regexp = "/^[a-zA-Z]$/", message = "检索首字母必须是一个字母")
+    private String firstLetter;
+    /**
+     * 排序
+     */
+    @NotNull
+    @Min(value = 0, message = "排序必须是大于等于0的整数")
+    private Integer sort;
 
 }

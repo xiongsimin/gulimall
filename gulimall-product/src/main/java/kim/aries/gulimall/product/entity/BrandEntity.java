bc00585dced77c73967c9569c82389ecff390632
aries <xiongsm@yonyou.com>
2021-05-11 23:48:41 +0800

    临时

--- gulimall-product/src/main/java/kim/aries/gulimall/product/entity/BrandEntity.java
+++ gulimall-product/src/main/java/kim/aries/gulimall/product/entity/BrandEntity.java
@@ -29,7 +29,7 @@ public class BrandEntity implements Serializable {
     /**
      * 品牌id
      */
-
+    //临时
     @NotNull(message = "修改必须指定商品Id", groups = {UpdateGroup.class})
     @Null(message = "新增不能指定商品Id", groups = {AddGroup.class})
     @TableId

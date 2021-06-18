9c83a2c5b716c72244be2c5b088e443739e02abd
aries <2248950919@qq.com>
2021-06-18 19:21:59 +0800

    测试es操作Java--保存

--- gulimall-search/src/main/java/kim/aries/gulimall/search/config/GulimallElasticSearchConfig.java
+++ gulimall-search/src/main/java/kim/aries/gulimall/search/config/GulimallElasticSearchConfig.java
@@ -1,6 +1,7 @@
 package kim.aries.gulimall.search.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -13,6 +14,17 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class GulimallElasticSearchConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+        /*builder.addHeader("Authorization", "Bearer " + TOKEN);
+        builder.setHttpAsyncResponseConsumerFactory(
+                new HttpAsyncResponseConsumerFactory
+                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));*/
+        COMMON_OPTIONS = builder.build();
+    }
 
     @Bean
     public RestHighLevelClient esRestClient() {
--- gulimall-search/src/test/java/kim/aries/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/kim/aries/gulimall/search/GulimallSearchApplicationTests.java
@@ -1,11 +1,21 @@
 package kim.aries.gulimall.search;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import kim.aries.common.to.MemberPrice;
+import kim.aries.gulimall.search.config.GulimallElasticSearchConfig;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.math.BigDecimal;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -13,6 +23,28 @@ public class GulimallSearchApplicationTests {
 
     @Autowired
     private RestHighLevelClient client;
+
+    /**
+     * 测试存储数据到ES
+     */
+    @Test
+    public void indexData() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("users");
+        indexRequest.id("1");
+//        indexRequest.source("username", "zhangsan", "age", "18", "gender", "男");
+        MemberPrice memberPrice = new MemberPrice();
+        memberPrice.setId(1L);
+        memberPrice.setName("会员价");
+        memberPrice.setPrice(new BigDecimal(580.0));
+        String jsonString = JSON.toJSONString(memberPrice);
+        //要保存的内容
+        indexRequest.source(jsonString, XContentType.JSON);
+        //执行操作
+        IndexResponse index = client.index(indexRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
+        //todo 提取有用的响应数据
+        System.out.println(index);
+    }
+
 
     @Test
     public void contextLoads() {
